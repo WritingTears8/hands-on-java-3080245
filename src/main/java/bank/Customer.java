24cbc8bc84a8a5306d5df61258d48c35e6c40f5b
WritingTears8 <159260071+WritingTears8@users.noreply.github.com>
2024-02-21 15:15:12 +0000

    210202024_2

--- src/main/java/bank/Customer.java
+++ src/main/java/bank/Customer.java
@@ -6,6 +6,7 @@ public class Customer {
   private String username;
   private String password;
   private int accountId;
+  private boolean authenticated;
 
   public Customer(int id, String name, String username, String password, int accountID){
     setLd(id);
@@ -13,6 +14,8 @@ public class Customer {
     setUsername(username);
     setPassword(password);
     setAccountId(accountID);
+    setAuthenticated(authenticated: false);
+
     
   }
 
@@ -54,6 +57,15 @@ public class Customer {
 
   public void setAccountId(int accountId) {
     this.accountId = accountId;
+  }
+
+
+  public boolean isAuthenticated() {
+    return this.authenticated;
+  }
+
+  public void setAuthenticated(boolean authenticated) {
+    this.authenticated = authenticated;
   }
 
 
